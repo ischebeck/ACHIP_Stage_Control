3bd58d1cc45228a4bb83a85c25d8fcd6482e084b
hermann_b <benedikt.hermann@psi.ch>
2018-01-17 14:38:49 +0100

    Ascii interface frame

--- Smaract/interfaces.py
+++ Smaract/interfaces.py
@@ -2,20 +2,23 @@
 from time import sleep
 import socket
 
-class hexapod(multiplePV):
+class hexapod():
     '''A class for hexapod control with ASCII Commands'''
     def __init__(self, name):
         '''TODO: add limits for setting'''
-        TCP_IP = '192.168.2.115'
-        TCP_PORT = 17494
+        TCP_IP = '129.129.217.74'
+        TCP_PORT = 2000
         self.soc = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
         self.soc.connect((TCP_IP, TCP_PORT))
         self.mainName = name
         self.axes = ['x', 'y', 'z', 'pitch', 'yaw', 'roll']
     
     def send(self,command):
-        self.soc.send(':'+command+'\n')           
-    
+        
+        self.soc.send(command)  
+        data = self.soc.recv(4096).decode('UTF-8')
+        print(data)
+        
     def connect(self, axis):
         ###
         return True
