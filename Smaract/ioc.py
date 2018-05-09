1f66b7bcf08a1066796f7fa04e4207757f7cb9b2
hermann_b <benedikt.hermann@psi.ch>
2018-05-09 10:55:23 +0200

    ioc test

--- Smaract/ioc.py
+++ Smaract/ioc.py
@@ -5,8 +5,8 @@ from pcaspy import Driver, SimpleServer
 prefix = 'SATSY01-DLAC080-DHXP:' #define PV’s for reading and setting the speed
 
 pvdb = {
-        'SMS': {'TYPE': 'str'},
-        'RMS': {'TYPE': 'str'},
+        'SMS': {'TYPE': str},
+        'RMS': {'TYPE': str},
         }
 
 class myDriver(Driver):
