9ebd4680b9cab7cb8c2b04d751b8f41eee54d296
hermann_b <benedikt.hermann@psi.ch>
2018-05-11 09:23:16 +0200

    ioc test

--- Smaract/ioc.py
+++ Smaract/ioc.py
@@ -1,6 +1,7 @@
 # -*- coding: utf-8 -*-
 import time
 from pcaspy import Driver, SimpleServer
+from interfaces import ECM, hexapod, smaractLinear
 
 prefix = 'SATSY01-DLAC080-DHXP:' #define PV’s for reading and setting the speed
 
@@ -11,26 +12,30 @@ pvdb = {
                 'value': 'init'},
         }
 
-class myDriver(Driver):
+class iocDriver(Driver):
 
     def __init__(self):
         super(myDriver, self).__init__()
+        self.ECM = ECM()
 
     def write(self, reason, msg):
         if reason == 'SMS': #if EPICS input SMS
             print('send command ', msg)     
             self.setParam(reason, msg)
-            #send to hexapod
+            #send to ECM
+            self.ECM.sendRaw(msg)
             
-            #read from hexapod
+            #read-back from hexapod
+            msg = self.ECM.ret
             self.setParam('RMS', msg)
             self.updatePVs()
         return True
 
 
+
 if __name__ == '__main__': #create PVs based on prefix and pvdb definition
     server = SimpleServer()
     server.createPV(prefix, pvdb)
-    driver = myDriver()
+    driver = iocDriver()
     while True:
         server.process(0.1) # process CA transactions
